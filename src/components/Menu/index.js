3426ce2280023c091b92c106661724ccb282b63d
Ricardo TB <ricardo.jucrist@hotmail.com>
2020-08-02 14:37:24 -0300

    backend

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,14 +1,14 @@
-import React from "react";
-import Logo from "../../assets/img/LogoMain.png";
-import { Link } from "react-router-dom";
-import Button from "./components/ButtonLink";
-import "./Menu.css";
+import React from 'react';
+import Logo from '../../assets/img/LogoMain.png';
+import { Link } from 'react-router-dom';
+import Button from './components/ButtonLink';
+import './Menu.css';
 
 const Menu = () => {
   return (
     <nav className="Menu">
       <a href="/">
-        <img className="Logo" src={Logo} to="logo" />
+        <img className="Logo" alt="logo" src={Logo} to="logo" />
       </a>
 
       <Button as={Link} className="ButtonLink" href="/cadastro/video">
--- src/pages/Cadastro/Categoria/index.js
+++ src/pages/Cadastro/Categoria/index.js
@@ -1,13 +1,13 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import PageDefault from "../../../components/PageDefault";
-import FormField from "../../../components/FormField";
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
+import FormField from '../../../components/FormField';
 
 function CadastroCategoria() {
   const valoresIniciais = {
-    nome: "",
-    descricao: "",
-    cor: ""
+    nome: '',
+    descricao: '',
+    cor: '',
   };
   const [categorias, setCategorias] = useState([]);
   const [values, setValues] = useState(valoresIniciais);
@@ -15,21 +15,30 @@ function CadastroCategoria() {
   const setValue = (key, valor) => {
     setValues({
       ...values,
-      [key]: valor
+      [key]: valor,
     });
   };
 
-  const handleChangeInput = e => {
+  const handleChangeInput = (e) => {
     const { value } = e.target;
-    setValue(e.target.getAttribute("name"), value);
+    setValue(e.target.getAttribute('name'), value);
   };
 
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     setCategorias([...categorias, values]);
 
     setValues(valoresIniciais);
   };
+
+  useEffect(() => {
+    const URL = 'http://localhost:8080/categorias';
+    fetch(URL).then(async (res) => {
+      const result = await res.json();
+      console.log([...result]);
+      setCategorias([...result]);
+    });
+  }, [values.nome]);
 
   return (
     <PageDefault>
@@ -60,12 +69,13 @@ function CadastroCategoria() {
           onChange={handleChangeInput}
         />
 
-        <button>Cadastrar</button>
+        <button type="submit">Cadastrar</button>
       </form>
 
+      {categorias.length === 0 && <div>Loading...</div>}
       <ul>
-        {categorias.map((categoria, index) => {
-          return <li key={`${categoria}${index}`}>{categoria.nome}</li>;
+        {categorias.map((categoria) => {
+          return <li key={`${categoria.nome}`}>{categoria.nome}</li>;
         })}
       </ul>
 
